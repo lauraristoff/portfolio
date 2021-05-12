a8a532508add432d5d91dec5e9c8f5ef66431a6e
lauraristoff <lauraristoff83@gmail.com>
2021-05-12 12:44:09 +0000

    update all files with css animations and javascript for mobile nav menu

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+/*
+Student Name: Laura Ristoff
+File Name: script.js
+Date: 05/11/2021
+*/
+
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "block";
+    }
+}
